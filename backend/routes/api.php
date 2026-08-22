399a213907535fbffc1f52790ee645fd28624420
Avinash <niceavi007@gmail.com>
2026-08-22 22:57:03 +0530

    feat: add versioned health api

---
+++ backend/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\HealthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/v1/health', HealthController::class);
